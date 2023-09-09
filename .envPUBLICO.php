2fb4117f927a0ecba51717923e414fa388114b4d
jmarquez-urq <jmarquez.urquiza@gmail.com>
2023-09-09 11:34:03 -0300

    Agrega ejemplo de archivo .env.php para enviar a repositorios compartidos

---
+++ .envPUBLICO.php
@@ -0,0 +1,11 @@
+<?php
+
+function credenciales() {
+    return [
+        'usuario' => 'juan',
+        'clave' => 'supersecreto',
+        'servidor' => 'localhost',
+        'base_de_datos' => 'conexion_php',
+    ];
+}
+
